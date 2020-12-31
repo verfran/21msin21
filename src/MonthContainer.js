b6830e7eaa86fba94c07ff0004c608f2fb8e2a22
Verkeys Francis <verfran@hotmail.com>
2020-12-31 20:54:08 +0530

    verse changed to 2 Cor:5:14,15

--- src/MonthContainer.js
+++ src/MonthContainer.js
@@ -25,7 +25,7 @@ class MonthContainer extends Component {
             jan: {
                 "name": "January",
                 "scriptures": [
-                    { "id": "Romans 5:5  ", "verse": "And hope does not put us to shame, because God’s love has been poured out into our hearts through the Holy Spirit, who has been given to us." },
+                    { "id": "2 Corinthians 5:14, 15", "verse": "For Christ’s love compels us, because we are convinced that one died for all, and therefore all died. 15 And he died for all, that those who live should no longer live for themselves but for him who died for them and was raised again." },
                     { "id": "John 13:34, 35", "verse": "A new command I give you: Love one another. As I have loved you, so you must love one another. 35 By this everyone will know that you are my disciples, if you love one another." },
                     { "id": "Colossians 3:12-14", "verse": "Therefore, as God’s chosen people, holy and dearly loved, clothe yourselves with compassion, kindness, humility, gentleness and patience. 13 Bear with each other and forgive one another if any of you has a grievance against someone. Forgive as the Lord forgave you. 14 And over all these virtues put on love, which binds them all together in perfect unity." },
                 ],
