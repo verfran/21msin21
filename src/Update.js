9ae513b9ae1fb5492a210534ee037f29093ca06f
Verkeys Francis <verfran@hotmail.com>
2021-01-04 22:12:59 +0530

    upload interface test

--- src/Update.js
+++ src/Update.js
@@ -1,43 +1,55 @@
 import React, { Component } from 'react';
+import { FormGroup, FormText, Button, Input } from 'reactstrap';
+
+const formStyle = {
+    padding: "30px",
+}
 
 class Update extends Component {
     constructor() {
         super();
         this.state = {
-            loggedIn: true,
-            isLoaded: false,
+            fileName: "",
+            selectedFile: null,
         }
     }
 
-    componentDidMount() {
-        this.fetchData()
+    onChooseFile = (event) => {
+        console.log(event.target.files[0])
+        this.setState(
+            {
+                fileName: event.target.files[0].name,
+                selectedFile: event.target.files[0]
+            }
+        )
     }
 
-    fetchData() {
-        this.setState({ isLoaded: false });
+    onUpload = (event) => {
+        if (this.state.fileName === "") {
+            alert("choose a file upload")
+        }
 
-        fetch('http://127.0.0.1:8000/api/msdata/verfran@hotmail.com/')
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    isLoaded: true,
-                    scripture: json.scripture,
-                })
-            });
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: this.state.selectedFile,
+        };
+        fetch(`https://ms21-backend.herokuapp.com/api/msfileupload/273/`, requestOptions)
+            .then(response => response.json())
     }
 
     render() {
-        if (!this.state.isLoaded) {
-            return (
-                <div>
-                    Loading ...
-                </div>
-            )
-        }
-
         return (
-            <div>
-                {this.state.scripture.application.notes}
+            <div style={formStyle}>
+                <FormGroup row>
+                    <Input type="file" name="file" id="exampleFile" onChange={this.onChooseFile} />
+                    <FormText color="muted">
+                        Choose the scripture image and press upload button.
+                </FormText>
+                </FormGroup>
+                <div style={{ padding: "15px" }}>
+                    <Button color="info" size="lg" block onClick={this.onUpload}>Upload</Button>
+                </div>
             </div>
         );
     }
