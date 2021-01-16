d5b431b4409399b6f2af3d7e56e32810554ebbbc
Verkeys Francis <verfran@hotmail.com>
2021-01-16 15:36:50 +0530

    refactor to enable correction and file upload

---
+++ src/Correction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GroupMain from './group/GroupMain';
+
+const Correction = (props) => {
+    return (
+        <div>
+            <GroupMain action="Correction"/>
+        </div>
+    );
+}
+
+export default Correction;
