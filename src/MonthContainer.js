44e6aec745c3806a624c4675d419733c64627056
Verkeys Francis <verfran@hotmail.com>
2021-01-22 18:47:03 +0530

    youtube videos from south region channel

--- src/MonthContainer.js
+++ src/MonthContainer.js
@@ -28,17 +28,17 @@ class MonthContainer extends Component {
                     {
                         "id": "2 Corinthians 5:14, 15",
                         "verse": "For Christ’s love compels us, because we are convinced that one died for all, and therefore all died. 15 And he died for all, that those who live should no longer live for themselves but for him who died for them and was raised again.",
-                        "videoID": "uokhp-mX4uQ",
+                        "videoID": "CoGuK78xjq4",
                     },
                     {
                         "id": "John 13:34, 35",
                         "verse": "A new command I give you: Love one another. As I have loved you, so you must love one another. 35 By this everyone will know that you are my disciples, if you love one another.",
-                        "videoID": "8NqbCwOu_iU",
+                        "videoID": "XmrUj2UU8RQ",
                     },
                     {
                         "id": "Colossians 3:12-14",
                         "verse": "Therefore, as God’s chosen people, holy and dearly loved, clothe yourselves with compassion, kindness, humility, gentleness and patience. 13 Bear with each other and forgive one another if any of you has a grievance against someone. Forgive as the Lord forgave you. 14 And over all these virtues put on love, which binds them all together in perfect unity.",
-                        "videoID": "a5ufJI7JMjw",
+                        "videoID": "w4cSHb4ZiAo",
                     },
                 ],
                 "application": {
